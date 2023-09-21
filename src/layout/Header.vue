d6df579c0f3e9aaf09eefe33f0c3cb7f9e9f5b84
Jamie McArdle <mc4rdle@gmail.com>
2023-09-21 17:37:31 +0800

    up

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,8 +1,3 @@
 <template>
-	<div class="relative w-full max-w-md rounded-md shadow-sm">
-	<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3" aria-hidden="true">
-		<svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd"></path></svg>
-	</div>
-	<input type="text" name="search" id="search" class="block !text-base w-full rounded-md border-0 py-2 px-6 pl-9 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 transition duration-300 ease-in-out sm:text-sm sm:leading-6" placeholder="Search">
-</div>
+	
 </template>
